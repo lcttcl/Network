527acbdf8a4b0420e904e20f98b776238d49f419
lct-windows <lichangtai19@163.com>
2020-05-19 00:04:02 +0800

    1.0

--- client.py
+++ client.py
@@ -1,27 +1,39 @@
-# -*- encoding: utf-8 -*-
-"""
----------------------------------------
-@File        :   client.py   
-@Modify Time :   2020/2/22 21:27           
-@Author      :   urchin_lct
-@Contact     :   lichangtai17@gmail.com
-@Version     :   0.0
----------------------------------------
-"""
+# # -*- encoding: utf-8 -*-
+# """
+# ---------------------------------------
+# @File        :   client.py
+# @Modify Time :   2020/2/22 21:27
+# @Author      :   urchin_lct
+# @Contact     :   lichangtai17@gmail.com
+# @Version     :   0.0
+# ---------------------------------------
+# """
+# import socket
+#
+#
+# def client(index):
+#     """create"""
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+#     """connect"""
+#     s.connect(('115.34.98.164', 8888))
+#
+#     print('Recv msg: %s, Client: %d' % (s.recv(1024), index))
+#     s.close()
+#
+#
+# if __name__ == '__main__':
+#     for i in range(10):
+#         client(i)
+
+
 import socket
 
-
-def client(index):
-    """create"""
-    s = socket.socket()
-
-    """connect"""
-    s.connect(('115.34.98.164', 8888))
-
-    print('Recv msg: %s, Client: %d' % (s.recv(1024), index))
-    s.close()
-
-
-if __name__ == '__main__':
-    for i in range(10):
-        client(i)
+HOST = '115.34.98.164'  # The remote host
+PORT = 50007  # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('Hello, world')
+data = s.recv(1024)
+s.close()
+print('Received', repr(data))
--- t0.py
+++ t0.py
@@ -10,29 +10,7 @@
 """
 # coding=utf-8
 import threading
-from time import sleep, ctime
+import time
+import os
 
 
-def sing():
-    for i in range(3):
-        print("正在唱歌...%d" % i)
-        sleep(1)
-
-
-def dance():
-    for i in range(3):
-        print("正在跳舞...%d" % i)
-        sleep(1)
-
-
-if __name__ == '__main__':
-    print('---开始---:%s' % ctime())
-
-    t1 = threading.Thread(target=sing)
-    t2 = threading.Thread(target=dance)
-
-    t1.start()
-    t2.start()
-
-    sleep(5)  # 屏蔽此行代码，试试看，程序是否会立马结束？
-    print('---结束---:%s' % ctime())
